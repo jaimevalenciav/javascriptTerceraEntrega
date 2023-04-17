77f01ab0e6b9332803635dddf1ecbb519018c2cf
jaimevalenciav <jaimevalenciav@gmail.com>
2023-04-17 10:32:54 -0400

    Agregar localStorage
    Agregar localStorage

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const templateCard = document.getElementById('template-card').content
 const templateFooter = document.getElementById('template-footer').content
 const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment()
+const mostrarCarrito = document.getElementById('mostrar-carrito')
+const mostrarBlanco = document.getElementById('mostrar-blancos')
+const mostrarTodos = document.getElementById('mostrar-todos')
+const carro = document.getElementById('carro')
 let carritoCompra = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+    if (localStorage.getItem('carritoCompra')) {
+        carritoCompra = JSON.parse(localStorage.getItem('carritoCompra'))
+        pintarCarrito()
+    }
 })
 
 cards.addEventListener('click', e =>{
@@ -30,7 +38,8 @@ const fetchData = async () => {
     
 }
 
-const pintarCard = data => {
+const pintarCard = data => { 
+    
     data.forEach(producto => {
         templateCard.querySelector(".titulo-producto").textContent = producto.title
         templateCard.querySelector(".precio").textContent = producto.precio
@@ -39,6 +48,7 @@ const pintarCard = data => {
         templateCard.querySelector(".unidades").textContent = producto.saldo
         templateCard.querySelector(".id").textContent = producto.id
         const clone = templateCard.cloneNode(true)
+        
         fragment.appendChild(clone)
     })
     cards.appendChild(fragment)
@@ -64,7 +74,19 @@ const setCarritoCompra = objeto => {
         producto.cantidad = carritoCompra[producto.id].cantidad + 1
     }
     carritoCompra[producto.id] = {...producto}
-    pintarCarrito()    
+    pintarCarrito() 
+    
+    Toastify({
+        text: "Producto agregado al carrito",
+        duration: 1000,
+        close: true,
+        gravity: "top", 
+        position: "right",
+        stopOnFocus: true,
+        style: {
+        background: "#009bb0",
+        }
+    }).showToast()   
 }
 
 const pintarCarrito = () => {
@@ -72,7 +94,8 @@ const pintarCarrito = () => {
     Object.values(carritoCompra).forEach(producto => {
         templateCarrito.querySelector('th').textContent = producto.id
         templateCarrito.querySelectorAll('td')[0].textContent = producto.title
-        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.saldo - 1
+        templateCarrito.querySelectorAll('td')[2].textContent = producto.cantidad
         templateCarrito.querySelector('.btn-info').dataset.id = producto.id
         templateCarrito.querySelector('.btn-warning').dataset.id = producto.id
         templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
@@ -81,6 +104,8 @@ const pintarCarrito = () => {
     })
     items.appendChild(fragment)
     pintarFooter()
+
+    localStorage.setItem('carritoCompra', JSON.stringify(carritoCompra))
 }
 
 const pintarFooter = () => {
@@ -105,24 +130,118 @@ const pintarFooter = () => {
     btnVaciar.addEventListener('click', () => {
         carritoCompra = {}
         pintarCarrito()
+        lanzarAlerta("Felicitaciones!, Se ha realizado la compra.", "success")
+        
     })
 }
+
+function lanzarAlerta(titulo, icon){
+    Swal.fire({
+        title: titulo,        
+        icon: icon,
+        //confirmButtonText: 'Aceptar',
+        showConfirmButton: false,
+        timer: 1000
+      })
+}
+
+
 const btnAccion = e => {
     if(e.target.classList.contains('btn-info')) {        
         const producto = carritoCompra[e.target.dataset.id]
-        producto.cantidad++
-        carritoCompra[e.target.dataset.id] = {...producto}
-        pintarCarrito()
+        if ((producto.saldo -1) > 0){
+            producto.cantidad++
+            producto.saldo--
+            carritoCompra[e.target.dataset.id] = {...producto}
+            pintarCarrito()
+            
+            Toastify({
+                text: "Producto aumentado en 1",
+                duration: 1000,
+                close: true,
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                background: "#009bb0",
+            }}).showToast()
+        }else{
+            Toastify({
+                text: "Sin unidades disponibles para el producto",
+                duration: 1000,
+                close: true,
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                background: "#b02300",
+            }}).showToast()
+        }
     }
 
     if(e.target.classList.contains('btn-warning')){
         const producto = carritoCompra[e.target.dataset.id]
         producto.cantidad--
+        if(producto.cantidad > 0){
+            producto.saldo++
+        }
+        
         if(producto.cantidad === 0){
             delete carritoCompra[e.target.dataset.id]
         }
         pintarCarrito()
+        
+        Toastify({
+            text: "Producto disminuído en el carrito",
+            duration: 1000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+            background: "#b02300",
+        }
+      }).showToast()
     }
 
     e.stopPropagation()
 }
+
+mostrarCarrito.addEventListener('click',() => {
+    carro.classList.toggle('ocultar') 
+})
+
+mostrarBlanco.addEventListener('click',() => {
+    cargaBlancos()    
+})
+
+const cargaBlancos = async () => {      
+    try {
+        const res = await fetch('api.json')
+        let data = await res.json()        
+        data2 = data.filter(producto => producto.color == 'blanco')        
+        console.log(data2)              
+        pintarCard(data2)
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+mostrarTodos.addEventListener('click',() => {
+    cargaTodos()    
+})
+
+const cargaTodos = async () => {      
+    try {
+        const res = await fetch('api.json')
+        const data = await res.json()             
+        pintarCard(data)
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+
+
